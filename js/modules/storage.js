ee88e1c699e519b778d5a5da1478bc002d2183e8
Matheus Ferreira <matheusfdantas03@gmail.com>
2025-09-23 17:01:43 -0300

    feat: implement StorageManager class for course management in local storage

---
+++ js/modules/storage.js
@@ -0,0 +1,43 @@
+export class StorageManager {
+  constructor(storageKey) {
+    this.storageKey = storageKey;
+  }
+
+  getCourses() {
+    const coursesJSON = localStorage.getItem(this.storageKey);
+    return coursesJSON ? JSON.parse(coursesJSON) : [];
+  }
+
+  getCourseById(id) {
+    return this.getCourses().find(course => course.id === id);
+  }
+
+  getCoursesByStatus(status) {
+    return this.getCourses().filter(course => course.status === status);
+  }
+
+  saveCourses(courses) {
+    localStorage.setItem(this.storageKey, JSON.stringify(courses));
+  }
+
+  addCourse(newCourse) {
+    const courses = this.getCourses();
+    courses.push(newCourse);
+    this.saveCourses(courses);
+  }
+
+  updateCourse(updatedCourse) {
+    const courses = this.getCourses();
+    const index = courses.findIndex(course => course.id === updatedCourse.id);
+    if (index !== -1) {
+      courses[index] = updatedCourse;
+      this.saveCourses(courses);
+    }
+  }
+
+  deleteCourse(id) {
+    let courses = this.getCourses();
+    courses = courses.filter(course => course.id !== id);
+    this.saveCourses(courses);
+  }
+}
